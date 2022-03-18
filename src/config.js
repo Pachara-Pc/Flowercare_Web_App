5d68a97b44a0c6d093514984cd547a86689df84d
Pachara-Pc <65880372+Pachara-Pc@users.noreply.github.com>
2022-03-18 11:16:22 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAmQqvk3DExDeiSb6jRBvG1XB3lNk1wA5w",
-  authDomain: "flowercare-sensor-e0476.firebaseapp.com",
-  databaseURL: "https://flowercare-sensor-e0476-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "flowercare-sensor-e0476",
-  storageBucket: "flowercare-sensor-e0476.appspot.com",
-  messagingSenderId: "942570087039",
-  appId: "1:942570087039:web:9abba4c4559fcbab45d54a"
+  apiKey: "xxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxxxxx",
+  databaseURL: "xxxxxxxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxx"
 };
